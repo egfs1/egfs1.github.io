172960db8418fc38b9643dd442f88dcc020b1aa9
Eduardo Gomes <eduardogomesfs4@gmail.com>
2023-03-13 13:42:07 -0300

    navbar responsive for mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,41 @@
-import { FiGithub } from 'react-icons/fi'
+import { Component } from 'react'
+import { FiBarChart, FiGithub, FiMenu, FiX } from 'react-icons/fi'
 import './NavbarStyles.css'
 
-export function Navbar(){
-    return(
-        <>
-            <nav>
-                <div className="content">
-                    <a href="https://github.com/egfs1">
-                        <FiGithub className='logo'/>
-                    </a>
+export class Navbar extends Component{
+    state = { clicked: false }
 
-                    <div>
-                        <ul id='navbar-list'>
-                            <li>
-                                <a href="index.html">Home</a>
-                            </li>
-                            <li>
-                                <a href="index.html">About</a>
-                            </li>
-                            <li>
-                                <a href="index.html">Contact</a>
-                            </li>
-                        </ul>
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked})
+    }
+
+    render(){
+        return(
+            <>
+                <nav>
+                    <div className="content">
+                        <a href="https://github.com/egfs1">
+                            <FiGithub className='icon'/>
+                        </a>
+    
+                        <div id="navbar-list" className={this.state.clicked ? "#navbar-list active" : "#navbar-list"} >
+                            <ul>
+                                <li>
+                                    <a className="active" href="/">Home</a>
+                                </li>
+                                <li>
+                                    <a href="/about">About</a>
+                                </li>
+                            </ul>
+                        </div>
+    
+                        <div id="mobile" onClick={this.handleClick}>
+                            <i children={this.state.clicked ? <FiX className='icon'/> : <FiMenu className='icon'/>}/>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </>
-    )
+                </nav>
+            </>
+        )
+
+    }
 }
